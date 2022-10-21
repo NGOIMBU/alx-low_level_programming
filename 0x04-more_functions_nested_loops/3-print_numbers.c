0b2ad981a07b17f62df8ec5eaa3f9851d7971f3d
Ngo <imbubeauty80@gmail.com>
2022-10-21 10:07:22 -0700

    New try

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,27 +1,18 @@
 #include "main.h"
-
 /**
- * print_alphabet_x10 - print alphabet
- *
+ * print_numbers - print numbers
  * Return: Always 0.
  */
-void print_alphabet_x10(void)
+void print_numbers(void)
 {
-int alphabet;
-int count;
+int i;
 
-
-count = 0;
-while (count < 10)
+i = 0;
+while (i < 10)
 {
-for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
+_putchar (i + '0');
+i++;
 }
 
-count++;
-_putchar('\n');
-}
-
-
+_putchar ('\n');
 }
