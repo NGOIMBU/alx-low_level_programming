d25744bc30885cc0157fece80641f04e8bc350d8
Ngo <imbubeauty80@gmail.com>
2022-10-24 16:26:00 -0700

    C lang

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _puts - prints a string
- * @str: the string
- *
- * Return: the lenght of a string
- */
+* _puts - prints a string
+* @str: the string
+*
+* Return: the lenght of a string
+*/
 void _puts(char *str);
 {
 int i = 0;
