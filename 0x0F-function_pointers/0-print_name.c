91a483cc7b0448f4a1975c554bf6137edd963680
Ngo <imbubeauty80@gmail.com>
2022-11-19 15:30:51 -0800

    new commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
 #include "function_pointers.h"
 
 /**
-*print_name - prints a name
-*@name: name to print
-*
-*Return: void
-*/
-void print_name(char *name, void (*f)(char *));
+ *print_name - prints a name
+ *@name: name to print
+ *@f: function to print with
+ *
+ *Return: void
+ */
+void print_name(char *name, void (*f)(char *))
 {
-printf("Hello, this is my %s/n", name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
