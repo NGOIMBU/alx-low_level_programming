b16cbedd918d82ea000a630d886c1a9f5e20a717
Ngo <imbubeauty80@gmail.com>
2022-11-30 10:03:15 -0800

    code more

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,6 +13,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *new;
 listint_t *temp;
 
+(void)temp;
+
+new = malloc(sizeof(listint_t));
+
 if(new == NULL)
 return(NULL);
 
